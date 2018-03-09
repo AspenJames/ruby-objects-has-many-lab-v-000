94d57f6317daae7a7dd420df63e1fb7a8204f2ad
Aspen James <aspenjames@tqca.org>
2018-03-09 22:52:19 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,6 @@
 class Artist
   attr_accessor :songs, :name
+  @@song_count = 0
 
   def initialize(name)
     @name = name
@@ -9,12 +10,14 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     song.artist = self
     @songs << song
+    @@song_count += 1
     song
   end
 
